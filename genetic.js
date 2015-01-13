32e762ff7640972b8d3592a17f3caecc68f03af2
yury.loskat <razoomnick@gmail.com>
2015-01-13 16:58:01 +0300

    Formatted.

--- genetic.js
+++ genetic.js
@@ -1,76 +1,81 @@
-function Genetic(populationSize, mutationValue, solver){
-  var population = new SortedFixedSizeArray(populationSize);
-  var probability = 2 / populationSize;
-  
-  for (var i = 0; i < populationSize; i++){
-    var randomSolution = solver.getRandomSolution();
-	var key = solver.getValue(randomSolution);
-	population.push({key: key, value: randomSolution});
+function Genetic(populationSize, mutationValue, solver) {
+  var population;
+  var probability;
+  init();
+
+  function init() {
+    population = new SortedFixedSizeArray(populationSize);
+    probability = 2 / populationSize;
+    for (var i = 0; i < populationSize; i++) {
+      var randomSolution = solver.getRandomSolution();
+      var key = solver.getValue(randomSolution);
+      population.push({ key: key, value: randomSolution });
+    }
   }
-  
-  this.step = function(){
-	var parent1 = getItemFromPopulation();
-	var parent2 = getItemFromPopulation();
-	while(parent2 == parent1){
-	  parent2 = getItemFromPopulation();
-	}
-	var child = solver.combine(parent1.value, parent2.value);
-	if (Math.random() < mutationValue){
-	  solver.mutate(child);
-	}
-	var key = solver.getValue(child);
-	population.push({key: key, value: child});
+
+  this.step = function () {
+    var parent1 = getItemFromPopulation();
+    var parent2 = getItemFromPopulation();
+    while (parent2 == parent1) {
+      parent2 = getItemFromPopulation();
+    }
+    var child = solver.combine(parent1.value, parent2.value);
+    if (Math.random() < mutationValue) {
+      solver.mutate(child);
+    }
+    var key = solver.getValue(child);
+    population.push({ key: key, value: child });
   };
-  
-  this.getBest = function(){
-	return population.getBest();
+
+  this.getBest = function () {
+    return population.getBest();
   };
-  
-  function getItemFromPopulation(){
-	var item = null;
-	var i = 0;
-	while (item == null){
-	  var rnd = Math.random();
-	  if (rnd < probability){
-		item = population.elements[i % population.elements.length];
-	  }
-	  i++;
-	}
-	return item;
+
+  function getItemFromPopulation() {
+    var item = null;
+    var i = 0;
+    while (item == null) {
+      var rnd = Math.random();
+      if (rnd < probability) {
+        item = population.elements[i % population.elements.length];
+      }
+      i++;
+    }
+    return item;
   }
 }
 
-function SortedFixedSizeArray(size){
+function SortedFixedSizeArray(size) {
   var maxKey = null;
   this.elements = [];
-  
-  this.push = function(element){
-    if (this.elements.length < size){
+
+  this.push = function (element) {
+    if (this.elements.length < size) {
       this.elements.push(element);
-	  return;
-	}
-	if (maxKey != null && maxKey <= element.key){
-	  return;
-	}
-	for (var i = 0; i < this.elements.length; i++){
-	  if (Math.abs((this.elements[i].key - element.key)) < 0.0001){
-	    return;
-	  }
-	}
-	this.elements.push(element);
-	this.elements = this.elements.sort(function(a, b) {
+      return;
+    }
+    if (maxKey != null && maxKey <= element.key) {
+      return;
+    }
+    for (var i = 0; i < this.elements.length; i++) {
+      if (Math.abs((this.elements[i].key - element.key)) < 0.0001) {
+        return;
+      }
+    }
+    this.elements.push(element);
+    this.elements = this.elements.sort(function (a, b) {
       var x = a.key; var y = b.key;
       return ((x < y) ? -1 : ((x > y) ? 1 : 0));
     });
-	this.elements.splice(size, 1);
-	maxKey = this.elements[size - 1].key;
+    this.elements.splice(size, 1);
+    maxKey = this.elements[size - 1].key;
   };
-  
-  this.getBest = function(){
-    this.elements = this.elements.sort(function(a, b) {
+
+  this.getBest = function () {
+    this.elements = this.elements.sort(function (a, b) {
       var x = a.key; var y = b.key;
       return ((x < y) ? -1 : ((x > y) ? 1 : 0));
     });
-	return this.elements[0];
+    return this.elements[0];
   };
 }
--- test.js
+++ test.js
@@ -1,83 +1,83 @@
-(function(){
+(function () {
   $("#btnGo").click(test);
-  
-  function test(){
+
+  function test() {
     var citiesCount = parseInt($("#txtCities").val());
-	var populationSize = parseInt($("#txtPopulation").val());
+    var populationSize = parseInt($("#txtPopulation").val());
     var mutationsValue = parseFloat($("#txtMutations").val());
-	var iterations = parseInt($("#txtIterations").val());
-	testInternal(citiesCount, populationSize, mutationsValue, iterations);
+    var iterations = parseInt($("#txtIterations").val());
+    testInternal(citiesCount, populationSize, mutationsValue, iterations);
   }
-  
-  function testInternal(citiesCount, populationSize, mutationsValue, iterations){
+
+  function testInternal(citiesCount, populationSize, mutationsValue, iterations) {
     var map = generateMap(citiesCount);
-	var salesman = new Salesman(map);
-	var genetic = new Genetic(populationSize, mutationsValue, salesman);
-	for (var i = 0; i < iterations; i++){
-	  genetic.step();
-	}
-	var bestGenetic = genetic.getBest();
-	var greedy = getGreedySolution(map);
-	var greedyScore = salesman.getValue(greedy);
-	draw(map, bestGenetic.value, "canvasGenetic");
-	draw(map, greedy, "canvasGreedy");
-	$("#lblGeneticResult").html(bestGenetic.key)
-	$("#lblGreedyResult").html(greedyScore);
+    var salesman = new Salesman(map);
+    var genetic = new Genetic(populationSize, mutationsValue, salesman);
+    for (var i = 0; i < iterations; i++) {
+      genetic.step();
+    }
+    var bestGenetic = genetic.getBest();
+    var greedy = getGreedySolution(map);
+    var greedyScore = salesman.getValue(greedy);
+    draw(map, bestGenetic.value, "canvasGenetic");
+    draw(map, greedy, "canvasGreedy");
+    $("#lblGeneticResult").html(bestGenetic.key);
+    $("#lblGreedyResult").html(greedyScore);
   }
-  
-  function generateMap(citiesCount){
-	var result = [];
-	for (var i = 0; i < citiesCount; i++){
-	  var x = Math.floor(Math.random() * 500);
-	  var y = Math.floor(Math.random() * 500);
-	  result.push({x: x, y: y});
-	}
-	return result;
+
+  function generateMap(citiesCount) {
+    var result = [];
+    for (var i = 0; i < citiesCount; i++) {
+      var x = Math.floor(Math.random() * 500);
+      var y = Math.floor(Math.random() * 500);
+      result.push({ x: x, y: y });
+    }
+    return result;
   }
-  
-  function draw(map, solution, canvasId){
-	var canvas = document.getElementById(canvasId);
+
+  function draw(map, solution, canvasId) {
+    var canvas = document.getElementById(canvasId);
     var ctx = canvas.getContext("2d");
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
-	ctx.strokeStyle = '#ff0000';
-	for (var i = 0; i < map.length; i++){
-	  ctx.beginPath();
-	  ctx.arc(map[i].x, map[i].y, 2, 0, 2*Math.PI);
-	  ctx.stroke();
-	}
-	ctx.strokeStyle = '#000000';
-	ctx.moveTo(map[solution[0]].x, map[solution[0]].y);
-	for (i = 1; i <= solution.length; i++){
-	  var point = map[solution[i % map.length]];
-	  ctx.lineTo(point.x, point.y);
-	}
-	ctx.stroke();
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.strokeStyle = '#ff0000';
+    for (var i = 0; i < map.length; i++) {
+      ctx.beginPath();
+      ctx.arc(map[i].x, map[i].y, 2, 0, 2 * Math.PI);
+      ctx.stroke();
+    }
+    ctx.strokeStyle = '#000000';
+    ctx.moveTo(map[solution[0]].x, map[solution[0]].y);
+    for (i = 1; i <= solution.length; i++) {
+      var point = map[solution[i % map.length]];
+      ctx.lineTo(point.x, point.y);
+    }
+    ctx.stroke();
   }
-  
-  function getGreedySolution(map){
+
+  function getGreedySolution(map) {
     var mapCopy = map.slice(0);
-	var result = [0];
-	mapCopy[0].used = 1;
-	var current = 0;
-	for (var i = 1; i < map.length; i++){
-	  var minDistance = null;
-	  var minPosition = null;
-	  for (var j = 0; j < mapCopy.length; j++){
-		if (!mapCopy[j].used){
-		  var a = mapCopy[current];
-		  var b = mapCopy[j];
-		  var distance = Math.sqrt(Math.pow(a.x - b.x, 2) + Math.pow(a.y - b.y, 2));
-		  if (!minDistance || distance < minDistance){
-		    minDistance = distance;
-			minPosition = j;
-		  }
-		}
-	  }
-	  result.push(minPosition);
-	  mapCopy[minPosition].used = 1;
-	  current = minPosition;
-	}
-	return result;
+    var result = [0];
+    mapCopy[0].used = 1;
+    var current = 0;
+    for (var i = 1; i < map.length; i++) {
+      var minDistance = null;
+      var minPosition = null;
+      for (var j = 0; j < mapCopy.length; j++) {
+        if (!mapCopy[j].used) {
+          var a = mapCopy[current];
+          var b = mapCopy[j];
+          var distance = Math.sqrt(Math.pow(a.x - b.x, 2) + Math.pow(a.y - b.y, 2));
+          if (!minDistance || distance < minDistance) {
+            minDistance = distance;
+            minPosition = j;
+          }
+        }
+      }
+      result.push(minPosition);
+      mapCopy[minPosition].used = 1;
+      current = minPosition;
+    }
+    return result;
   }
-  
+
 })();
